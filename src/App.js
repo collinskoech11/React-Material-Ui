97e6fb41f1b16a8d5799a68a853fe75322d03307
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-03-14 13:51:54 +0300

    import components from material ui

--- src/App.js
+++ src/App.js
@@ -7,6 +7,24 @@ import Checkbox from '@material-ui/core/Checkbox'
 import SaveIcon from '@material-ui/icons/Save'
 import DeleteIcon from '@material-ui/icons/Delete'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
+import TextField from '@material-ui/core/TextField'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #333, #999',
+    border:0,
+    borderRadius: 15,
+    color: 'white',
+    padding: '0 30px'
+  }
+})
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Text Styled Button</Button>
+}
 
 function CheckboxExample() {
   const [checked, setChecked ] = React.useState(true)
@@ -15,6 +33,8 @@ function CheckboxExample() {
       <FormControlLabel 
         control={<Checkbox
         checked={checked}
+        icon={<DeleteIcon/>}
+        checkedIcon={<SaveIcon/>}
         onChange={(e) => setChecked(e.target.checked)}
         inputProps={{
           'aria-label': 'primary checkbox'
@@ -35,6 +55,14 @@ function App() {
     <div className="App">
     
       <header className="App-header">
+      <ButtonStyled/>
+      <TextField
+        variant="outlined"
+        color="secondary"
+        type="email"
+        label="time"
+        placeholder="test@gmail.com"
+      />
       <CheckboxExample/>
       <ButtonGroup>
         <Button startIcon={<SaveIcon/>} size="large" variant="contained" color="primary">Save</Button>
