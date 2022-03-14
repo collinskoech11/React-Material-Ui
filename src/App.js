d66a03b121d62a58f9f68ee70fa8e660aa0d5152
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-03-14 15:12:20 +0300

    added a grid container

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import React from 'react'
 import './App.css';
-import Button from '@material-ui/core/Button'
-import ButtonGroup from '@material-ui/core/ButtonGroup'
-import Checkbox from '@material-ui/core/Checkbox'
-import SaveIcon from '@material-ui/icons/Save'
-import DeleteIcon from '@material-ui/icons/Delete'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Checkbox from '@material-ui/core/Checkbox';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import TextField from '@material-ui/core/TextField';
 
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, ThemeProvider, createTheme } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
+
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import  Paper  from '@material-ui/core/Paper';
+import  Grid  from '@material-ui/core/Grid';
+
+import 'fontsource-roboto';
 
 const useStyles = makeStyles({
   root: {
-    background: 'linear-gradient(45deg, #333, #999',
+    background: 'linear-gradient(45deg, #FE6BBB, #FFBE53)',
     border:0,
+    fontWeight: 'bold',
     borderRadius: 15,
+    marginBottom:15,
     color: 'white',
-    padding: '0 30px'
+    padding: '15px 20px',
+  }
+})
+
+const theme = createTheme({
+  typography:{
+    h2: {
+      fontSize: 36,
+      marginBottom: 15,
+    }
+  },
+  palette: {
+    primary: {
+      main: green[500],
+    },
+    secondary: {
+      main: green[500],
+    }
   }
 })
 
@@ -51,26 +78,36 @@ function CheckboxExample() {
 
 function App() {
   return (
-    
-    <div className="App">
-    
-      <header className="App-header">
-      <ButtonStyled/>
-      <TextField
-        variant="outlined"
-        color="secondary"
-        type="email"
-        label="time"
-        placeholder="test@gmail.com"
-      />
-      <CheckboxExample/>
-      <ButtonGroup>
-        <Button startIcon={<SaveIcon/>} size="large" variant="contained" color="primary">Save</Button>
-        <Button startIcon={<DeleteIcon/>} size="large" variant="contained" color="secondary">Discard</Button>
-      </ButtonGroup>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Container maxWidth="xs">
+      <ThemeProvider theme={theme}>
+        <div className="App">
+        
+          <header className="App-header">
+          <Typography variant="h2">Welcome to MUI</Typography>
+          <Typography variant="subtitle1">Learn How to use Material ui</Typography>
+          <ButtonStyled/>
+          <TextField
+            variant="outlined"
+            color="secondary"
+            type="email"
+            label="time"
+            placeholder="test@gmail.com"
+          />
+          <Grid container>
+            <Grid item>
+              <Paper style={{ height:75, width:50, }}/>
+            </Grid>
+          </Grid>
+          <CheckboxExample/>
+          <ButtonGroup>
+            <Button startIcon={<SaveIcon/>} size="large" variant="contained" color="primary">Save</Button>
+            <Button startIcon={<DeleteIcon/>} size="large" variant="contained" color="primary">Discard</Button>
+          </ButtonGroup>
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+        </div>
+      </ThemeProvider>
+    </Container>
   );
 }
 
