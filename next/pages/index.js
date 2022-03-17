028e311b731bef7bc2a4693713bf45c211908e9b
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-03-17 13:01:20 +0300

    dynamic article links

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
 
 export default function Home({articles}) {
   console.log(articles);
@@ -8,13 +9,13 @@ export default function Home({articles}) {
         <title>Home</title>
         <meta name='keywords' content='web development, programming' />
       </Head>
-       <h1>Welcome to Next</h1>
+       <ArticleList articles={articles}/>
     </div>
   )
 }
 
 
-export const getSTaticProps = async () => {
+export const getStaticProps = async () => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`);
   const articles = await res.json();
 
