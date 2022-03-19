bf4dce4e35078844a53ee0e0938998b31295009c
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-03-19 11:57:14 +0300

    added remix app

---
+++ my-remix-app/app.arc
@@ -0,0 +1,20 @@
+@app
+my-remix-app-c4cc
+
+@http
+/*
+  method any
+  src server
+
+@static
+
+@tables
+user
+  pk *String
+
+password
+  pk *String # userId
+
+note
+  pk *String  # userId
+  sk **String # noteId
--- next/data.js
+++ next/data.js
@@ -30,4 +30,12 @@ export const articles = [
       body:
         'We can’t get enough of our mobile apps. There were 204 billion apps downloads last year, and that number is rising in 2020.  When app stores entered the mainstream tech culture, they exposed developers to an audience of millions who were keen to adopt the innovative capabilities',
     },
+    {
+      id: '5',
+      title: 'Why open ecosystems are the future of app development',
+      excerpt:
+        'When app stores entered the mainstream tech culture, they exposed developers to an audience of millions...',
+      body:
+        'We can’t get enough of our mobile apps. There were 204 billion apps downloads last year, and that number is rising in 2020.  When app stores entered the mainstream tech culture, they exposed developers to an audience of millions who were keen to adopt the innovative capabilities',
+    },
   ]
